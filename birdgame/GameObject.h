39ad31ec0e8ea24a81378b50979e6181cfb54d20
smittens <smittens@gmail.com>
2012-05-15 16:57:49 -0700

    Finish gameobject

--- birdgame/GameObject.h
+++ birdgame/GameObject.h
@@ -7,7 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CCSprite.h"
 
 @interface GameObject : NSObject
+{
+	CGPoint delta;
+	
+	CCSprite *sprite;
+}
+
+@property CGPoint delta;
+@property (nonatomic, retain) CCSprite *sprite;
+
+@property CGPoint origin;
+@property CGPoint center;
+@property CGRect rect;
+@property (readonly) CGRect collisionRect;
+
+- (BOOL) collidesWithPoint:(CGPoint)p;
+- (BOOL) collidesWithObject:(GameObject *)object;
+
+- (void) update;
 
 @end
--- birdgame/GameObject.m
+++ birdgame/GameObject.m
@@ -9,5 +9,64 @@
 #import "GameObject.h"
 
 @implementation GameObject
+@synthesize delta, sprite;
+
+- (void) setOrigin:(CGPoint)newO
+{
+	sprite.position = newO;
+}
+
+- (CGPoint) origin
+{
+	return sprite.position;
+}
+
+- (void) setCenter:(CGPoint)c
+{
+	sprite.position = CGPointMake(c.x - sprite.contentSize.width/2, c.y - sprite.contentSize.height/2);
+}
+
+- (CGPoint) center
+{
+	return CGPointMake(sprite.position.x + sprite.contentSize.width/2, sprite.position.y + sprite.contentSize.height/2);
+}
+
+- (void) setRect:(CGRect)newRect
+{
+	self.origin = newRect.origin;
+	sprite.contentSize = newRect.size;
+}
+
+- (CGRect) rect
+{
+	CGPoint origin = self.origin;
+	return CGRectMake(origin.x, origin.y, sprite.contentSize.width, sprite.contentSize.height);
+}
+
+- (CGRect) collisionRect
+{
+	return self.rect;
+}
+
+- (BOOL) collidesWithPoint:(CGPoint)p
+{
+	return CGRectContainsPoint(self.collisionRect, p);
+}
+
+- (BOOL) collidesWithObject:(GameObject *)object
+{
+	return CGRectIntersectsRect(self.collisionRect, object.collisionRect);
+}
+
+- (void) update
+{
+	sprite.position = CGPointMake(sprite.position.x + delta.x, sprite.position.y + delta.y);
+}
+
+- (void) dealloc
+{
+	self.sprite = nil;
+	[super dealloc];
+}
 
 @end
