18de071aa2dca96c6b75a75294d109500666c1a5
Taylor Savage <tjsavage@stanford.edu>
2012-05-15 16:14:52 -0700

    Initial commit

---
+++ birdgame/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  birdgame
+//
+//  Created by Taylor Savage on 5/15/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
