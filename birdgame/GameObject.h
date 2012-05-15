66834290d3ca5c82e7cbd738d06ebdbcc4e5c4cb
smittens <smittens@gmail.com>
2012-05-15 16:24:09 -0700

    Adding gameobject

---
+++ birdgame/GameObject.h
@@ -0,0 +1,13 @@
+//
+//  GameObject.h
+//  birdgame
+//
+//  Created by Will Perl on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameObject : NSObject
+
+@end
---
+++ birdgame/GameObject.m
@@ -0,0 +1,13 @@
+//
+//  GameObject.m
+//  birdgame
+//
+//  Created by Will Perl on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameObject.h"
+
+@implementation GameObject
+
+@end
